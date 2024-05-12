07bf666864871aa60cebb1839682611aa8979eba
shiyangzhang <shiyangzhang@whale.im>
2024-05-12 12:25:30 +0800

    [main]

--- main.zig
+++ main.zig
@@ -2,20 +2,7 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    const a: u8 = 1;
-    const b: *const u8 = &a;
-    const c = &b;
-    print("value: {any}\n", .{c});
+    var a = "hello".*;
+    a[0] = 'j';
+    std.debug.print("{s}\n", .{a});
 }
-// pub fn main() !void {
-//     const a: u8 = 1;
-//     const b: ?*const u8 = &a;
-//     print("value: {any}\n", .{b});
-
-//     if (b) |*c| {
-//         print("value: {}, refer: {}\n", .{ c, c.* });
-//     }
-//     if (b) |c| {
-//         print("value: {}, refer: {}\n", .{ c, c.* });
-//     }
-// }
--- note.md
+++ note.md
@@ -1,8 +1,25 @@
 https://zig.guide/language-basics/errors
 
-https://www.youtube.com/watch?v=QIUjP8DILDU&list=PLtB7CL7EG7pCw7Xy1SQC53Gl8pI7aDg9t&index=6
+https://www.youtube.com/watch?v=4j7MkMc0sN0&list=PLtB7CL7EG7pCw7Xy1SQC53Gl8pI7aDg9t&index=9
 
 @ques zig 怎么处理内存的
+
+## 2024-05-12 11:35:59
+
+```zig
+// @ques 有没有办法不是enum,实现typeName
+const Number = union {
+    int: u8,
+    float: f64,
+    fn typeName(self: Number) []const u8 {
+        if (self == .int) {
+            return "int";
+        }
+        return "float";
+    }
+};
+
+```
 
 ## 2024-05-11 11:54:17
 
