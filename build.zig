852329193fdb163f1b9f6c98337cf170d81f013b
shiyangzhang <shiyangzhang@whale.im>
2024-05-22 14:27:51 +0800

    [main]

--- build.zig
+++ build.zig
@@ -1,98 +1,38 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-learn",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // This adds a command line option to zig build via the -D flag. i.e. -Dloop
+    const use_loop = b.option(bool, "loop", "Use non-recursive Fibonacci") orelse false;
+    // Now we can make a build decision based on that option.
+    const fib_file: []const u8 = if (use_loop) "src/fib_loop.zig" else "src/fib_recurse.zig";
+    const fibonacci = b.addModule("fibonacci", .{ .root_source_file = .{ .path = fib_file } });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    // To pass on this option to your program, use addOptions.
+    const options = b.addOptions();
+    options.addOption(bool, "use_loop", use_loop);
 
     const exe = b.addExecutable(.{
-        .name = "zig-learn",
-        .root_source_file = b.path("src/main.zig"),
+        .name = "34_build",
+        .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    exe.root_module.addOptions("config", options);
+    exe.root_module.addImport("fibonacci", fibonacci);
+
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
+    // Add a step to run the built executable.
     const run_cmd = b.addRunArtifact(exe);
-
-    const ziglyph = b.dependency("ziglyph", .{
-        .optimize = optimize,
-        .target = target,
-    });
-    // for exe, lib, tests, etc.
-    exe.root_module.addImport("ziglyph", ziglyph.module("ziglyph"));
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
+    // To run the app, you have to install it first.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
+    // Pass any command line args after `--` to the executable.
+    if (b.args) |args| run_cmd.addArgs(args);
+    // The actual run step.
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,30 @@
 const std = @import("std");
 
-const ziglyph = @import("ziglyph");
-const letter = ziglyph.letter;
-const number = ziglyph.number;
+// Import the module containing passed-in build
+// command line options.
+const config = @import("config");
+// Import the fibonacci module selected via
+// build command line option.
+const fib = @import("fibonacci").fib;
 
 pub fn main() !void {
-    std.debug.print("{}", .{ziglyph.isLetter('z')});
+    // Executable command line args processing.
+    // These are the ones after the `--`.
+    var args_iter = std.process.args();
+    _ = args_iter.next(); // program name
+
+    // Let's get the actual number.
+    const n: usize = if (args_iter.next()) |arg|
+        try std.fmt.parseInt(usize, arg, 10) // Parse the arg as an usize, base 10.
+    else
+        7; // Default to 7 if no arg provided.
+
+    // Use the build command line option via the `config` module.
+    const fib_type: []const u8 = if (config.use_loop) "loop" else "recursive";
+    // Print the nth Fibonacci number.
+    std.debug.print("\n\n {}th fibonacci ({s}): {} \n\n", .{
+        n,
+        fib_type,
+        fib(n),
+    });
 }
