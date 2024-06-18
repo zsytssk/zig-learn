f68cabd32b4ddf36ac87d9a0077daadd061382bc
shiyangzhang <shiyangzhang@whale.im>
2024-06-18 15:15:14 +0800

    [main]

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,9 @@
 const std = @import("std");
-// const c = @import("./c.zig");
-const Point = @import("./point.zig");
 
 pub fn main() anyerror!void {
-    const p1 = Point.new(1.0, 1.0);
-    const p2 = Point.new(2.0, 2.0);
-    const p3 = p1.add(&p2);
-    std.debug.print("{any}:{}\n", .{ @TypeOf(p3), p3 });
-
-    var p4 = Point.origin;
-    p4.increment();
-    std.debug.print("{}\n", .{p4});
-    std.debug.print("{}\n", .{Point.origin});
+    var map = std.StringHashMap(u8).init(std.heap.page_allocator);
+    try map.put("hell", 1);
+    try map.putNoClobber("hell", 3);
+    const a = map.get("hell") orelse 9;
+    std.debug.print("{any}\n", .{a});
 }
